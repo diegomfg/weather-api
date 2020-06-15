5f663f565fcc8cb6013d5d4ebf506c6ba8ac47c4
Diego Matheus <diegoamatheus@gmail.com>
2020-06-15 12:59:20 -0400

    refactoring

--- react/src/App.js
+++ react/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-import Api from "./Components/Api";
+import API from "./Components/Api";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Api />
+        <API />
       </header>
     </div>
   );
